55a9e140755613593a2e067b1cf4281f299cc25e
Yang Liu <yliu12@mit.edu>
2022-03-21 15:39:43 -0400

    Initial Commit

---
+++ AmbientLight/AmbientLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmbientLightApp.swift
+//  AmbientLight
+//
+//  Created by Yang Liu on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct AmbientLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
